39f7f8e1f732796f65f381a5a3bcd21663def665
ulypka <36298288+ulypka@users.noreply.github.com>
2025-03-04 16:37:34 +0300

    Add files via upload

---
+++ lem.py
@@ -0,0 +1,21 @@
+#sasha  -s 1000 -g 38 -f 20
+import argparse, sys
+parser = argparse.ArgumentParser()
+parser.add_argument('-g', help="Final gradus of lemonchelo 35 is default", default=35,type=int)
+parser.add_argument('-k', help="Gradus of spirt 95 is default", default=95,type=int)
+parser.add_argument('-s', help="V of spirt", type=int)
+parser.add_argument('-f', help="procent os sugar in final 30 is default", default=30,type=int)
+
+args = parser.parse_args()
+if (args.s is None):
+    parser.print_usage()
+    print ('%s Error: need V of spirt'%(sys.argv[0]))
+    quit()
+g=args.g #final gradus
+k=args.k #spirt gradus
+s=args.s #V spirta
+f=args.f #prosent of sugar
+c=s*f*k/(100*g) #sugar in gram
+print c,'sugar gram'
+v=(s*k-g*c*60/100-g*s)/g
+print v,'water'
