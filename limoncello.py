6854e61ea7f879eaf6ad968418003f91b5beee97
ulypka <36298288+ulypka@users.noreply.github.com>
2025-09-02 22:28:43 +0300

    Update limoncello.py
    add dextrose option

--- limoncello.py
+++ limoncello.py
@@ -4,7 +4,7 @@ parser.add_argument('-g', help="Final gradus of lemonchelo 35 is default", defau
 parser.add_argument('-k', help="Gradus of spirt 95 is default", default=95,type=int)
 parser.add_argument('-s', help="V of spirt", type=int)
 parser.add_argument('-f', help="procent os sugar in final 15 is default", default=15,type=int)
-
+parser.add_argument('-d', help="dextrose", action='store_true')
 args = parser.parse_args()
 if (args.s is None):
     parser.print_usage()
@@ -15,6 +15,10 @@ k=args.k #spirt gradus
 s=args.s #V spirta
 f=args.f #prosent of sugar
 c=s*f*k/(100*g) #sugar in gram
-print c,'sugar gram'
+if args.d:
+    c=c/0.7
+    print (c,'dextrose gram')
+else:
+    print (c,'sugar gram')
 v=(s*k-g*c*60/100-g*s)/g
 print v,'water'
